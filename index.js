19e3964df606491730cb6f62b9df39c2312a756c
abdurrahman720 <personal.abdurrahman@gmail.com>
2022-12-26 12:49:19 +0600

    both json dataset has been created and respond sent successfully

--- index.js
+++ index.js
@@ -5,9 +5,16 @@ const port = process.env.PORT || 5222;
 
 app.use(cors());
 
+const courses = require('./data/course-name.json')
+
 app.get('/', (req, res) => {
     res.send('Courses API Running');
 })
+
+app.get('/courses',(req, res) => {
+    res.send(courses);
+})
+
 
 app.listen(port, () => {
     console.log('Listening on port ' + port);
