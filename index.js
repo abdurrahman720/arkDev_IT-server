82cf507b5226a47f1fa9d4095fa8c9d5593610f5
abdurrahman720 <personal.abdurrahman@gmail.com>
2022-12-26 08:39:00 +0600

    cors installed

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5222;
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Courses API Running');
